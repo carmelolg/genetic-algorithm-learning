606771550da4b805fec20b9d34d21f70b2493c0c
carmelolg <carmelolagamba@hotmail.it>
2021-04-07 12:20:02 +0200

    - minor fix

--- Constants.py
+++ Constants.py
@@ -4,7 +4,7 @@ from string import ascii_letters
 class Constants:
     population_number = 1000
     mutation_threshold = 0.18
-    max_length = 16
-    solution = 'Carmelo La Gamba'
+    solution = 'I\'m produced by a genetic algorithm!'
+    max_length = len(solution)
     generations = 50000
-    all_chars = ascii_letters + ' '
+    all_chars = ascii_letters + ' \'!?*&.;,-'
